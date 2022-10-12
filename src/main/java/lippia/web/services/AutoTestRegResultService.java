162db462ec4a0958fc0c9fcc355f6e324ca606d2
Luchofag <luciano.fag95@gmail.com>
2022-10-12 16:09:10 -0300

    Update AutoTestRegResultService.java

--- src/main/java/lippia/web/services/AutoTestRegResultService.java
+++ src/main/java/lippia/web/services/AutoTestRegResultService.java
@@ -8,10 +8,10 @@ import org.openqa.selenium.WebElement;
 public class AutoTestRegResultService extends ActionManager {
 
     private static WebElement visibleRegMailError() {
-        return getElement(AutoTestConstants.LOG_VALIDATE_TXT_XPATH);
+        return getElement(AutoTestConstants.REG_VALIDATE_MAIL_TXT_XPATH);
     }
     private static WebElement visibleRegPassError() {
-        return getElement(AutoTestConstants.LOG_VALIDATE_TXT_XPATH);
+        return getElement(AutoTestConstants.REG_VALIDATE_PASS_TXT_XPATH);
     }
     /*public static void verifyErrorMailVisibility(){
         ActionManager.waitVisibility(AutoTestConstants.REG_VALIDATE_MAIL_TXT_XPATH);
